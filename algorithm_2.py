946e9c356337eb0beb6c371de7b12ee3ecb5e093
Cata1406 <m.ibanez@uniandes.edu.co>
2023-11-16 08:39:35 -0500

    Cambio

--- algorithm_2.py
+++ algorithm_2.py
@@ -11,9 +11,10 @@ def vertex_cover_2(G: SimpleGraph):
     Returns a vertex cover of the graph.
     """
     cover = set()
-    while G.E:
-        u = max(G.V, key=G.degree)
+    G2 = G.copy()
+    while G2.E:
+        u = max(G2.V, key=G2.degree)
         cover.add(u)
-        G.V.remove(u)
-        G.E = [e for e in G.E if u not in e]
+        G2.V.remove(u)
+        G2.E = [e for e in G2.E if u not in e]
     return cover
